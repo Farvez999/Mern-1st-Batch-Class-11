5cfcdae6fe137898c61049cd2b9d872b68f31444
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-03-03 22:30:07 +0600

    first commit

---
+++ dom/js/dom.js
@@ -0,0 +1,18 @@
+// console.log('Out side js file link');
+
+//Get Element by Tag name
+// const liCollaction = document.getElementsByTagName('li')
+// for (const li of liCollaction) {
+//     console.log(li.innerText);
+// }
+
+//Get Element by Class name
+// const ulCollactions = document.getElementsByClassName('title')
+// console.log(ulCollactions);
+// for (const ul of ulCollactions) {
+//     console.log(ul.innerText);
+// }
+
+//Get Element by Id name
+const subTitle = document.getElementById('sub-title')
+console.log(subTitle.innerText);
